7fab1200dc1d017dcc317ec994acad4315249184
LucianeEloiza <88690088+LucianeEloiza@users.noreply.github.com>
2021-10-15 10:49:14 -0300

    Questão 2

---
+++ principal2.java
@@ -0,0 +1,30 @@
+package QUESTÃO2;
+
+public class principal2 {
+
+	public static void main(String[] args) {
+		post Post = new post ("Na vida o plantio é opcional, porém, a colheita é obrigatória. "
+				+ "Por isso tome cuidado com o que você planta porque o que você plantar, "
+				+ "você vai colher.", "https://driveandlisten.herokuapp.com/");
+		
+		
+	Post.curtir(3);
+	System.out.println ("o número de curtidas são " +Post.getNumerocurtidas());
+	
+
+	Post.compartilhar(2);
+	System.out.println ("o número de compartilhamento são " +Post.getNumerocompartilhamentos());
+	
+		
+		
+		
+
+	}
+
+	
+
+	}
+
+
+	
+
